24d6f22f1bdb3bdcf03fabb194fb30a45a0d600d
Moti Bartov <moti.bartov@intenthq.com>
2025-05-08 17:21:47 +0300

    Flutter testing

--- src/lib/model/ProfileTest.ts
+++ src/lib/model/ProfileTest.ts
@@ -1,6 +1,16 @@
-interface ProfileTest { 
+interface TestProfile{
+    platform: string,
+    package_name: string;
+    activity_name: string;
+    automation_name: string;
+    tests: Test[];
+    quit_on_fail: boolean;
+}
+
+interface Test { 
     name: string;
     description: string;
     testPath: string;
     enabled: boolean;
+    quit_on_fail: boolean
 }
