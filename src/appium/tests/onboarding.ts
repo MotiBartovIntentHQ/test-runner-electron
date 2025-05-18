70a09923f28ea212ec4dc7fbbba973cdaa033564
Yanivper <154807986+Yanivper@users.noreply.github.com>
2025-05-18 14:24:31 +0300

    OnBoarding test

---
+++ src/appium/tests/onboarding.ts
@@ -0,0 +1,72 @@
+import { log } from "console";
+import { BaseTest, TestResult, TestStatus } from "../core/base_test.js";
+import * as fs from "fs";
+
+export default class OnboardingTest extends BaseTest {
+  constructor() {
+    super("OnboardingTest", "Verify OnBoarding status ");
+  }
+
+
+  async execute(driver: WebdriverIO.Browser): Promise<TestResult> {
+    this.eventEmitter.log("OnBoarding Test");
+
+    try {
+      const currentDir = process.cwd();
+
+      
+      await this.startOnboardingFromMenu(driver);
+      await driver.pause(5000);
+      const logs: string = fs.readFileSync(`${currentDir}/logcat_dump.txt`, "utf8");
+
+     
+
+      let status = TestStatus.PASS;
+      const expectedLogIndicator = "DuringOnboarding"; // or the exact phrase to match
+
+if (!logs.includes(expectedLogIndicator)) {
+  status = TestStatus.FAIL;
+  return {
+    test: this.name,
+    description: this.description,
+    status: status,
+    error: `Expected onboarding status log "${expectedLogIndicator}" not found.`,
+  };
+} else {
+  this.eventEmitter.log(`✅ Found onboarding status change: "${expectedLogIndicator}"`);
+}
+
+      return {
+        test: this.name,
+        description: this.description,
+        status: status,
+        error: "",
+      };
+    } catch (error: any) {
+      return {
+        test: this.name,
+        description: this.description,
+        status: TestStatus.FAIL,
+        error: JSON.stringify(error),
+      };
+    }
+  }
+
+  async startOnboardingFromMenu(driver: WebdriverIO.Browser) {
+    await this.clickThreeDots(driver)
+    const lastMenuItem = await driver.$(
+        'android=new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().text("OnBoarding Start"))'
+      );
+      await lastMenuItem.click(); // Click the item after scrolling
+  }
+
+  async clickThreeDots(driver: WebdriverIO.Browser){
+    try {
+        const overflowMenu = await driver.$("~More options"); // Default content-desc for 3-dots menu
+        await overflowMenu.click();
+        console.log("✅ 3-Dots Overflow Menu Clicked!");
+      } catch (error) {
+        console.error("❌ Could not find Overflow Menu:", error);
+      }
+  }
+}
