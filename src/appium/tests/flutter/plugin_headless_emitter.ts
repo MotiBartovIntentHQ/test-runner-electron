292da713b5a1f4d04d5dbf124369d985308cd339
Moti Bartov <moti.bartov@intenthq.com>
2025-05-21 14:24:06 +0300

    Plugin headless BackgroundEventEmitter test

---
+++ src/appium/tests/flutter/plugin_headless_emitter.ts
@@ -0,0 +1,89 @@
+import { log } from "console";
+import { BaseTest, TestResult, TestStatus } from "../../core/base_test.js";
+
+export default class FlutterHeadlessEventEmitter extends BaseTest {
+  constructor() {
+    super("FlutterHeadlessEventEmitter", "Flutter headless BackgroundEventEmitter test");
+  }
+
+
+  async execute(driver: WebdriverIO.Browser): Promise<TestResult> {
+    this.eventEmitter.log("FlutterPluginEventEmitter test");
+
+    try {
+      const logs = this.logs();
+      
+      let status = TestStatus.PASS;
+    
+      const backgroundEmitterOnCampaignTriggered = "BackgroundEventEmitter 2 main INFO onNewMessage: com.anagog.jedai.anagog_api.model.Message$OnCampaignTriggered";
+
+      if(!logs.includes(backgroundEmitterOnCampaignTriggered) ){
+        status = TestStatus.FAIL
+        return {
+          test: this.name,
+          description: this.description,
+          status: status,
+          error: "Unable to find Plugin BackgroundEventEmitter events prompts",
+        };
+      }
+
+      const internalMethodChannel = "[INTERNAL_METHOD_CHANNEL] onMessageCall: PluginInternalMessage#eventEmitterMessage";
+      const internalMethodChannelEventEmitterMessage = "[INTERNAL_METHOD_CHANNEL]: sdkEventEmitterMessage: {campaign_identifier:";
+  
+      if(!logs.includes(internalMethodChannel) || !logs.includes(internalMethodChannelEventEmitterMessage)){
+        status = TestStatus.FAIL
+        return {
+          test: this.name,
+          description: this.description,
+          status: status,
+          error: "Unable to find Plugin internalMethodChannel events prompts",
+        };
+      }
+
+      const headlessCallbackCampaignTrigger = "[anagogHeadlessCallback] ON_CAMPAIGN_TRIGGERED";
+
+      if(!logs.includes(headlessCallbackCampaignTrigger) ){
+        status = TestStatus.FAIL
+        return {
+          test: this.name,
+          description: this.description,
+          status: status,
+          error: "Unable to find headlessCallbackCampaignTrigger events prompts",
+        };
+      }
+
+
+      return {
+        test: this.name,
+        description: this.description,
+        status: TestStatus.PASS,
+        error: "",
+      };
+    } catch (error: any) {
+      return {
+        test: this.name,
+        description: this.description,
+        status: TestStatus.FAIL,
+        error: JSON.stringify(error),
+      };
+    }
+  }
+
+  async forceSendPeriodicReport(driver: WebdriverIO.Browser) {
+    await this.clickThreeDots(driver)
+    const lastMenuItem = await driver.$(
+        'android=new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().text("Force Send Periodic Reports"))'
+      );
+      await lastMenuItem.click(); // Click the item after scrolling
+  }
+
+  async clickThreeDots(driver: WebdriverIO.Browser){
+    try {
+        const overflowMenu = await driver.$("~More options"); // Default content-desc for 3-dots menu
+        await overflowMenu.click();
+        console.log("✅ 3-Dots Overflow Menu Clicked!");
+      } catch (error) {
+        console.error("❌ Could not find Overflow Menu:", error);
+      }
+  }
+}
