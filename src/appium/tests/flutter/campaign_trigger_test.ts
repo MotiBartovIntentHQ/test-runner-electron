5a1eccd0dfb5b5fc2d0173048b45943afca3ef4e
Moti Bartov <moti.bartov@intenthq.com>
2025-05-21 13:37:23 +0300

    SDK and FlutterPlugin Android EventEmitter tests

--- src/appium/tests/flutter/campaign_trigger_test.ts
+++ src/appium/tests/flutter/campaign_trigger_test.ts
@@ -1,6 +1,6 @@
 import { log } from "console";
 import { BaseTest, TestResult, TestStatus } from "../../core/base_test.js";
-
+import { NativeDriverHolder } from "./native_driver_holder.js";
 export default class CampaignTriggerTest extends BaseTest {
   constructor() {
     super("CampaignTriggerTest", "Verifying campaign trigger");
@@ -10,13 +10,18 @@ export default class CampaignTriggerTest extends BaseTest {
   async execute(driver: WebdriverIO.Browser): Promise<TestResult> {
     this.eventEmitter.log(`Execute Campaign Trigger Test`)
 
+        let nativeDriver = await NativeDriverHolder.getInstance()
+    
     try {
-      //await driver.pause(5000);
-      //await driver.background(10);
-      //await driver.activateApp('com.anagog.jema.flutter2.sampleapp');
-      await driver.pause(5000);
+      // await nativeDriver.background(2);
+      const  currentPackage = await nativeDriver.getCurrentPackage();
+      this.eventEmitter.log(`Current native pacakge: ${currentPackage}`)
+      await nativeDriver.execute('mobile: backgroundApp');
+      await nativeDriver.pause(2000);
+      await nativeDriver.execute('mobile: activateApp', {'appId': currentPackage});
 
-      const currentDir = process.cwd();
+      await driver.pause(10000)
+      
       const logs = this.logs();
 
       let testStatus = TestStatus.FAIL;
